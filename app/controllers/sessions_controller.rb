05bff64b9b6a81b543d9b4de5e9650c8ed1a5e0d
Seth Baughman <sbaughman1@me.com>
2016-04-25 19:35:11 -0400

    add methods in sessions controller for new, create, and destroy

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,24 @@
 class SessionsController < ApplicationController
+
+  def new
+  end
+
+  def create
+    user = User.find_by(email: params[:session][:email])
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user
+      flash[:success] = "Logged in successfully"
+      redirect_to :root
+    else
+      flash[:danger] = "Incorrect email / password combo"
+      render :new
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    flash[:success] = "Logged out successfully"
+    redirect_to :root
+  end
+
 end
