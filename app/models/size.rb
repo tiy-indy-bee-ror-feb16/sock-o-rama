ccccf326d28a5ae868b90a265ecea824bd806d40
J-Henry <JH@JH.local>
2016-04-26 13:38:13 -0400

    Complete size test

--- app/models/size.rb
+++ app/models/size.rb
@@ -1,7 +1,12 @@
 class Size < ApplicationRecord
-
-  VALID_SIZES = %w(S M L XL)
-
+  VALID_SIZES = %w(XXS XS S M L XL XXL XXXL).freeze
   validates :size, presence: true
   validates_inclusion_of :size, in: VALID_SIZES
+  before_validation :uppercase
+
+  private
+
+  def uppercase
+    self.size.upcase! unless self.size.nil?
+  end
 end
--- test/models/size_test.rb
+++ test/models/size_test.rb
@@ -3,48 +3,46 @@ require 'test_helper'
 class SizeTest < ActiveSupport::TestCase
 
   def setup
-    @size = Size.new()
+    @size = Size.new
   end
 
   # Test good paramaters
-  # Better to use X.valid? or x.save ??
+
   test "valid small size" do
     @size.attributes = { size: "S" }
     assert @size.save
     assert_equal "S", @size.size
-    assert_equal 1, Size.count
   end
 
   test "valid medium size" do
     @size.attributes = { size: "M" }
     assert @size.save
     assert_equal "M", @size.size
-    assert_equal 1, Size.count
   end
 
   test "valid large size" do
     @size.attributes = { size: "L" }
     assert @size.save
     assert_equal "L", @size.size
-    assert_equal 1, Size.count
   end
 
   test "valid extra large size" do
     @size.attributes = { size: "XL" }
     assert @size.save
     assert_equal "XL", @size.size
-    # assert_equal 1, Size.count
+  end
+
+  test "capitalize invalid small size" do
+    @size.attributes = { size: "s" }
+    @size.save
+    assert @size.valid?, "Saved size as #{@size.size}"
+    assert_equal "S", @size.size
   end
 
   # Test bad patamaters
 
   test "invalid missing size" do
     @size.attributes = {}
-    refute @size.save
-  end
-
-  test "invalid small size" do
-    @size.attributes = { size: "s" }
     refute @size.save
   end
 
@@ -60,6 +58,11 @@ class SizeTest < ActiveSupport::TestCase
 
   test "invalid extra large size" do
     @size.attributes = { size: "X" }
+    refute @size.save
+  end
+
+  test "invalid blank size" do
+    @size.attributes = { size: "" }
     refute @size.save
   end
 end
