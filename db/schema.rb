70a68cd4d7a612833c00af331d37ad716df09cc2
Seth Baughman <sbaughman1@me.com>
2016-04-26 14:04:48 -0400

    add category column to socks

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160425205433) do
+ActiveRecord::Schema.define(version: 20160426180252) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -69,6 +69,7 @@ ActiveRecord::Schema.define(version: 20160425205433) do
     t.string   "material"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "category"
   end
 
   create_table "users", force: :cascade do |t|
