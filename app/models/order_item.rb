d70186cb21b9b8915f081c003adbc17bad4f370d
firestorm451 <firestorm451@users.noreply.github.com>
2016-04-26 13:33:26 -0400

    Update order_item.rb

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,7 +1,5 @@
 class OrderItem < ApplicationRecord
   belongs_to :order
-  has_many :socks
-  has_many :sock_sizes, through: :socks
-  has_many :sizes, through: :sock_sizes # <-- Is this necessary?
-  validates :socks, presence: true
+  has_many :sock_sizes
+  validates :quantity, presence: true, numericality: true
 end
