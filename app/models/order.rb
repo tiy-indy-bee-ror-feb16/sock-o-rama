ddb26256d8d6f41e3dacbe674aba1c815d29152b
firestorm451 <firestorm451@users.noreply.github.com>
2016-04-26 13:28:36 -0400

    Update order.rb

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,7 +2,7 @@ class Order < ApplicationRecord
 
   belongs_to :address # and address belongs to user, user has many addresses will take care of the rest of the relationship?
   has_many :order_items
-  validates :order_item, :order_total, presence: true
-  validates :order_total, numericality: true
+  validates :order_item, :total, presence: true
+  validates :total, numericality: true
 
 end
