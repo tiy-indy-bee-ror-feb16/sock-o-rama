8a9f60b67b2ea15bf9909c4abce7f087413c1bab
Kurt Friedrich <kurt.is.friedrich@gmail.com>
2016-04-25 21:52:53 -0400

    add address model relationships and validations

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,2 +1,11 @@
 class Address < ApplicationRecord
+  belongs_to :user
+  validates :street, :city, :state, :zip, presence: true
+  validates_inclusion_of :state, in: Carmen::state_codes('US')
+  validates :zip, format: { with: /[0-9]\{5\}(-[0-9]\{4\})?/,
+    message: "please enter a valid 5-digit zip code" }
+
+  def address
+    [street, city, state, zip].join(',')
+  end
 end
