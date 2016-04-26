8005935f8070b220a7f387c18c5719eb72d9a963
Ryder Timberlake <wrtimberlake@gmail.com>
2016-04-26 06:13:54 -0400

    Order validations and relationships

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,10 @@
 class Order < ApplicationRecord
+
+  belongs_to :address # and address belongs to user, user has many addresses will take care of the rest of the relationship?
+  has_many :socks
+  has_many :sock_sizes, through: :socks
+  has_many :sizes, through: :sock_sizes # <-- Is this necessary?
+  validates :order_items, :order_total, presence: true
+  validates :order_total, numericality: true
+
 end
