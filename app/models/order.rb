2f241939cace438e8ae9689e2a6321876847499a
Seth Baughman <sbaughman1@me.com>
2016-04-27 16:59:26 -0400

    add tax and total methods to Order

--- app/models/order.rb
+++ app/models/order.rb
@@ -4,14 +4,32 @@ class Order < ApplicationRecord
   has_many :order_items
   validates :order_item, :total, presence: true
   validates :total, numericality: true
-  before_save :update_subtotal
+  before_save :update_subtotal, :update_tax, :update_total
 
   def subtotal
     order_items.map { |item| item.valid? ? (item.quantity * item.price) : 0 }.sum
   end
 
+  def tax
+    subtotal * 0.7
+  end
+
+  def total
+    subtotal + tax
+  end
+
+  private
+
   def update_subtotal
     self[:subtotal] = subtotal
+  end
+
+  def update_tax
+    self[:tax] = tax
+  end
+
+  def update_total
+    self[:total] = total
   end
 
 end
