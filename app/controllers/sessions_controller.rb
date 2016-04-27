7face0da8631f9312fbca5cb08d198bab6532d7e
Kurt Friedrich <kurt.is.friedrich@gmail.com>
2016-04-27 17:25:28 -0400

    add user login functionality

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
   def create
     user = User.find_by(email: params[:session][:email])
     if user && user.authenticate(params[:session][:password])
-      session[:user_id] = user
+      session[:user_id] = user.id
       flash[:success] = "Logged in successfully"
       redirect_to :root
     else
