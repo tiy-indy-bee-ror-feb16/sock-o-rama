11019fdbd3b20eeda7ff13b30bcab66e7dbde311
Seth Baughman <sbaughman1@me.com>
2016-04-27 13:16:05 -0400

    add method to SockImage to create picture attachment from URL

--- app/models/sock_image.rb
+++ app/models/sock_image.rb
@@ -1,6 +1,11 @@
 class SockImage < ApplicationRecord
   belongs_to :sock
   validates_presence_of :url
+  before_save :picture_from_url
   has_attached_file :picture, styles: { medium: "300x300>", thumb: "100x100>" }
   validates_attachment_content_type :picture, content_type: /\Aimage\/.*\Z/
+
+  def picture_from_url
+    self.picture = URI.parse(url)
+  end
 end
