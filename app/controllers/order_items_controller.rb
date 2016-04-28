b5d222ce284dc294a43490500dde85d946aa913d
Seth Baughman <sbaughman1@me.com>
2016-04-28 11:51:36 -0400

    remove belongs_to address relationship for Order. Create js views for cart updates. Changed some styling on cart_row

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -11,16 +11,14 @@ class OrderItemsController < ApplicationController
   end
 
   def update
-    @order_item = @order.order_items.find(params[:item_id])
+    @order_item = @order.order_items.find(params[:id])
     @order_item.update_attributes(order_item_params)
     @order_items = @order.order_items
-    redirect_to :back
   end
 
   def destroy
-    @order.order_items.find(params[:item_id]).destroy
+    @order.order_items.find(params[:id]).destroy
     @order_items = @order.order_items
-    redirect_to :back
   end
 
   private
