c37694234f6b74335f2fc290f99c57d3fa4447ef
Seth Baughman <sbaughman1@me.com>
2016-04-28 16:40:12 -0400

    Added new address form

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -1,2 +1,9 @@
 class AddressesController < ApplicationController
+
+  def new
+    @address = current_user.address || Address.new
+    redirect_to edit_address_path(@address) if current_user.address
+  end
+
+  def create
 end
