a75e199d99181d6001f8c31b4ee0d6f1f688c71e
J-Henry <JH@JH.local>
2016-04-28 12:05:31 -0400

    Syntax refactoring

--- config/routes.rb
+++ config/routes.rb
@@ -8,5 +8,5 @@ Rails.application.routes.draw do
   get '/login'  => "sessions#new", as: :login
   post '/login' => "sessions#create", as: :login_create
   get '/logout' => "sessions#destroy", as: :logout
-  get '/search' => 'search#index', as: :search
+  get '/search' => "search#index", as: :search
 end
