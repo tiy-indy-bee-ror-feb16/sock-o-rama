0044447cbd277ac1061716cc3adcfffa3d6b1082
Seth Baughman <sbaughman1@me.com>
2016-04-29 10:25:58 -0400

    added name to address validations and added name field to address form

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -22,7 +22,7 @@ class AddressesController < ApplicationController
   private
 
   def address_params
-    params.require(:address).permit(:street, :city, :zip, :state)
+    params.require(:address).permit(:street, :city, :zip, :state, :name)
   end
 
 end
